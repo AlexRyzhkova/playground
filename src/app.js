bdd70286ae2ac5546724afeb9c255e515ca39a99
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-08-05 12:41:04 +0200

    Add a bundle

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import { createToggleButton } from "./components/toggleButton.js";
-import { createTitle } from "./components/title.js";
-
-const title = createTitle();
-const toggleButton = createToggleButton();
-
-document.body.append(title);
-document.body.append(toggleButton);
